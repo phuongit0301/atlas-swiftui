653b3d6030bc94b08c74a8afcef33b99e7f434b6
Phan Thanh Phuong <phuong@accumulus.sg>
2023-05-15 10:43:00 +0700

    Initial Commit

---
+++ ATLAS/ATLASApp.swift
@@ -0,0 +1,17 @@
+//
+//  ATLASApp.swift
+//  ATLAS
+//
+//  Created by phuong phan on 15/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct ATLASApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
