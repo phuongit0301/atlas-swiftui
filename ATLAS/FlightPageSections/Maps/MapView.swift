4eb3999593c17304dd676bc2ff6cce68585168d3
Phan Thanh Phuong <phuong@accumulus.sg>
2023-08-26 17:25:02 +0700

    Done Map with Polyline and annotation

--- ATLAS/FlightPageSections/Maps/MapView.swift
+++ ATLAS/FlightPageSections/Maps/MapView.swift
@@ -9,51 +9,164 @@ import SwiftUI
 import CoreLocation
 import MapKit
 
-struct AnnotatedItem: Identifiable {
-    let id = UUID()
-    var name: String
-    var coordinate: CLLocationCoordinate2D
-}
+//struct AnnotatedItem: MKAnnotation {
+//    let id = UUID()
+//    var name: String
+//    var coordinate: CLLocationCoordinate2D
+//}
+//
+//struct MapViewModal: View {
+//
+//    @StateObject var locationViewModel = LocationViewModel()
+//    @State private var mapType: MKMapType = .standard
+//
+////    var parent = MapView(mapType: $mapType)
+//
+//    var body: some View {
+//        VStack {
+//            MapView(mapType: $mapType)
+//                .ignoresSafeArea()
+//                .overlay {
+//
+//                    Button(action: {
+//                        var parent = MapView(mapType: $mapType)
+//                        parent.testFunc()
+//                    }) {
+//                        VStack {
+//                            Spacer()
+//                            HStack {
+//                                Spacer()
+//                                ZStack {
+//                                    RoundedRectangle(cornerRadius: 5)
+//                                        .frame(width: 40, height: 40)
+//                                        .foregroundColor(.white)
+//
+//                                    Image(systemName: "map.fill")
+//                                        .font(.system(size: 30))
+//                                        .foregroundColor(.gray)
+//
+//                                }
+//                                .padding(.trailing, 10)
+//                            }
+//                            Spacer()
+//                        }
+//                    }
+//                }
+//        }
+//        .onAppear {
+//            if locationViewModel.authorizationStatus == .notDetermined {
+//                locationViewModel.requestPermission()
+//            }
+//        }
+//    }
+//}
+//
+//struct MapView: UIViewRepresentable {
+//    @Binding var mapType: MKMapType
+//
+//    func makeCoordinator() -> Coordinator {
+//        Coordinator(self)
+//    }
+//
+//    class Coordinator: NSObject, MKMapViewDelegate {
+//        var parent: MapView
+//
+//        init(_ parent: MapView) {
+//            self.parent = parent
+//        }
+//
+//        func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+//            //print(mapView.centerCoordinate)
+//        }
+//
+//        func mapView(_ mapView: MKMapView,
+//                     didUpdate userLocation: MKUserLocation) {
+//            print("User location\(userLocation.coordinate)")
+//        }
+//
+//        func mapViewWillStartLoadingMap(_ mapView: MKMapView) {
+//            print("Map will start loading")
+//        }
+//
+//        func mapViewDidFinishLoadingMap(_ mapView: MKMapView) {
+//            print("Map did finish loading")
+//        }
+//
+//        func mapViewWillStartLocatingUser(_ mapView: MKMapView) {
+//            print("Map will start locating user")
+//        }
+//
+//        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+//            let view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: nil)
+//            view.canShowCallout = true
+//            return view
+//        }
+//    }
+//
+//    func makeUIView(context: Context) -> MKMapView {
+//
+//        let region: MKCoordinateRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: -37.8136, longitude: 144.9631), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+//
+//        let mapView = MKMapView()
+//        mapView.delegate = context.coordinator
+//        mapView.region = region
+//
+//        mapView.showsScale = true
+//        mapView.setUserTrackingMode(MKUserTrackingMode.followWithHeading, animated: true)
+//        mapView.userTrackingMode = MKUserTrackingMode.followWithHeading
+//
+//        mapView.showsUserLocation = true
+//        //mapView.showsCompass = false
+//
+//        return mapView
+//    }
+//
+//    func updateUIView(_ view: MKMapView, context: Context) {
+//        view.mapType = mapType
+//    }
+//
+//    func testFunc()  {
+//        print("Toggle Compass")
+//    }
+//}
+//
+//class LocationViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
+//    @Published var authorizationStatus: CLAuthorizationStatus
+//
+//    private let locationManager: CLLocationManager
+//
+//    override init() {
+//        locationManager = CLLocationManager()
+//        authorizationStatus = locationManager.authorizationStatus
+//
+//        super.init()
+//        locationManager.delegate = self
+//        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+//        locationManager.startUpdatingLocation()
+//    }
+//
+//    func requestPermission() {
+//        locationManager.requestWhenInUseAuthorization()
+//    }
+//
+//    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+//        authorizationStatus = manager.authorizationStatus
+//    }
+//}
+//
 
 struct MapViewModal: View {
-
+    @ObservedObject var modelView = MapViewModel()
     @StateObject var locationViewModel = LocationViewModel()
-    @State private var mapType: MKMapType = .standard
-    
-//    var parent = MapView(mapType: $mapType)
 
     var body: some View {
         VStack {
-            MapView(mapType: $mapType)
-                .ignoresSafeArea()
-                .overlay {
-
-                    Button(action: {
-                        var parent = MapView(mapType: $mapType)
-                        parent.testFunc()
-                    }) {
-                        VStack {
-                            Spacer()
-                            HStack {
-                                Spacer()
-                                ZStack {
-                                    RoundedRectangle(cornerRadius: 5)
-                                        .frame(width: 40, height: 40)
-                                        .foregroundColor(.white)
-
-                                    Image(systemName: "map.fill")
-                                        .font(.system(size: 30))
-                                        .foregroundColor(.gray)
-
-                                }
-                                .padding(.trailing, 10)
-                            }
-                            Spacer()
-                        }
-                    }
-                }
-        }
-        .onAppear {
+            MapView(
+                region: modelView.region,
+                lineCoordinates: modelView.lineCoordinates,
+                annotation: modelView.pointsOfInterest
+            )
+        }.onAppear {
             if locationViewModel.authorizationStatus == .notDetermined {
                 locationViewModel.requestPermission()
             }
@@ -62,113 +175,68 @@ struct MapViewModal: View {
 }
 
 struct MapView: UIViewRepresentable {
-    @Binding var mapType: MKMapType
+    let region: MKCoordinateRegion
+    let lineCoordinates: [CLLocationCoordinate2D]
+    let annotation: [MKAnnotation]
+    
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+//        mapView.region = region
+        mapView.setRegion(region, animated: true)
+        mapView.showsUserLocation = true
+        mapView.showsScale = true
+        
+        mapView.addAnnotations(annotation)
+        let polyline = MKPolyline(coordinates: lineCoordinates, count: lineCoordinates.count)
+        mapView.addOverlay(polyline)
+        
+        mapView.delegate = context.coordinator
+        mapView.userTrackingMode = MKUserTrackingMode.follow
+        
+        return mapView
+    }
+    
+    func updateUIView(_ view: MKMapView, context: Context) {}
     
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
-
-    class Coordinator: NSObject, MKMapViewDelegate {
-        var parent: MapView
-
-        init(_ parent: MapView) {
-            self.parent = parent
-        }
-
-        func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
-            //print(mapView.centerCoordinate)
-        }
-
-        func mapView(_ mapView: MKMapView,
-                     didUpdate userLocation: MKUserLocation) {
-            print("User location\(userLocation.coordinate)")
-        }
-
-        func mapViewWillStartLoadingMap(_ mapView: MKMapView) {
-            print("Map will start loading")
-        }
-
-        func mapViewDidFinishLoadingMap(_ mapView: MKMapView) {
-            print("Map did finish loading")
-        }
-
-        func mapViewWillStartLocatingUser(_ mapView: MKMapView) {
-            print("Map will start locating user")
-        }
-
-        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-            let view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: nil)
-            view.canShowCallout = true
-            return view
-        }
-    }
-
-    func makeUIView(context: Context) -> MKMapView {
-
-        let region: MKCoordinateRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: -37.8136, longitude: 144.9631), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
-
-        let mapView = MKMapView()
-        mapView.delegate = context.coordinator
-        mapView.region = region
-
-        mapView.showsScale = true
-        mapView.setUserTrackingMode(MKUserTrackingMode.followWithHeading, animated: true)
-        mapView.userTrackingMode = MKUserTrackingMode.followWithHeading
-
-        mapView.showsUserLocation = true
-        //mapView.showsCompass = false
-
-        return mapView
-    }
-
-    func updateUIView(_ view: MKMapView, context: Context) {
-        view.mapType = mapType
-    }
-
-    func testFunc()  {
-        print("Toggle Compass")
-    }
 }
 
-class LocationViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
-    @Published var authorizationStatus: CLAuthorizationStatus
-
-    private let locationManager: CLLocationManager
-
-    override init() {
-        locationManager = CLLocationManager()
-        authorizationStatus = locationManager.authorizationStatus
-
-        super.init()
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.startUpdatingLocation()
+class Coordinator: NSObject, MKMapViewDelegate {
+    var parent: MapView
+    
+    init(_ parent: MapView) {
+        self.parent = parent
     }
-
-    func requestPermission() {
-        locationManager.requestWhenInUseAuthorization()
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        // If the annotation is the user dot then return nil
+        if annotation is MKPointAnnotation {
+            return nil
+        }
+        
+        // Create an annotation view
+        let annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: "business")
+        
+        annotationView.image = UIImage(named: "pin.circle.fill")
+        annotationView.canShowCallout = false
+        annotationView.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        //        annotationView.isHidden = true
+        return annotationView
+        
     }
-
-    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
-        authorizationStatus = manager.authorizationStatus
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if let routePolyline = overlay as? MKPolyline {
+            let renderer = MKPolylineRenderer(polyline: routePolyline)
+            renderer.strokeColor = UIColor.systemBlue
+            renderer.lineWidth = 10
+            return renderer
+        }
+        return MKOverlayRenderer()
     }
 }
-//
-//struct MapView: View {
-//    private var pointsOfInterest = [
-//        AnnotatedItem(name: "Times Square", coordinate: .init(latitude: 40.75773, longitude: -73.985708)),
-//        AnnotatedItem(name: "Flatiron Building", coordinate: .init(latitude: 40.741112, longitude: -73.989723)),
-//        AnnotatedItem(name: "Empire State Building", coordinate: .init(latitude: 40.748817, longitude: -73.985428))
-//        ]
-//
-//    @State private var region: MKCoordinateRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 40.75773, longitude: -73.985708), span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
-//
-//    var body: some View {
-//        Map(coordinateRegion: $region, annotationItems: pointsOfInterest) { item in
-//            MapMarker(coordinate: item.coordinate, tint: .red)
-//        }.edgesIgnoringSafeArea(.all)
-//    }
-//}
 
 //struct MapView_Previews: PreviewProvider {
 //    static var previews: some View {
--- ATLAS/Model/MapViewModel.swift
+++ ATLAS/Model/MapViewModel.swift
@@ -5,8 +5,8 @@
 ////  Created by phuong phan on 25/08/2023.
 ////
 //
-//import SwiftUI
-//import MapKit
+import SwiftUI
+import MapKit
 //
 //class MapViewModel: NSObject, ObservableObject {
 //    
@@ -98,3 +98,98 @@
 //        .resume()
 //    }
 //}
+
+struct IMapModel: Identifiable, Decodable {
+    var id = UUID()
+    var name: String
+    var coord: String
+}
+
+let DATA_MAP = [
+    IMapModel(name: "KIGOB", coord: "N1306.8 E10051.1"),
+    IMapModel(name: "PASVA", coord: "N0615.5 E10204.5"),
+    IMapModel(name: "NUFFA", coord: "N0253.7 E10338.5"),
+    IMapModel(name: "PIBAP", coord: "N0230.4 E10406.3"),
+    IMapModel(name: "PASPU", coord: "N0159.3 E10406.3"),
+    IMapModel(name: "NYLON", coord: "N0136.9 E10406.4"),
+    IMapModel(name: "POSUB", coord: "N0127.4 E10407.8"),
+    IMapModel(name: "SANAT", coord: "N0107.8 E10359.5"),
+]
+
+class MapViewModel: ObservableObject {
+    @Published var region: MKCoordinateRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 1.988333, longitude: 104.105), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+    @Published var lineCoordinates = [CLLocationCoordinate2D]()
+    @Published var pointsOfInterest = [MKAnnotation]()
+    
+    init() {
+        var temp = [MKPointAnnotation]()
+        var temp1 = [CLLocationCoordinate2D]()
+        
+        for item in DATA_MAP {
+            let coord = convertCoordinates(item.coord)
+            let annotation = MKPointAnnotation()
+            annotation.title = item.name
+            annotation.coordinate = coord
+            
+            temp.append(annotation)
+            temp1.append(coord)
+        }
+        self.lineCoordinates = temp1
+        self.pointsOfInterest = temp
+    }
+}
+
+func convertCoordinates(_ coordinateString: String) -> CLLocationCoordinate2D {
+    let components = coordinateString.components(separatedBy: " ")
+    
+    if components.count == 2 {
+        let latitudeComponent = components[0]
+        let longitudeComponent = components[1]
+        
+        let latDegree = String(latitudeComponent.dropFirst())
+        let lonDegree = String(longitudeComponent.dropFirst())
+        
+        let latDegreeIndex = latDegree.index(latDegree.startIndex, offsetBy: 2)
+        let lonDegreeIndex = lonDegree.index(lonDegree.startIndex, offsetBy: 3)
+        
+        if let latDegrees = Double(latDegree[..<latDegreeIndex]),
+           let latMinutes = Double(latDegree[latDegreeIndex...]),
+           let lonDegrees = Double(lonDegree[..<lonDegreeIndex]),
+           let lonMinutes = Double(lonDegree[lonDegreeIndex...]) {
+            
+            let decimalLat = latDegrees + (latMinutes / 60)
+            let decimalLon = lonDegrees + (lonMinutes / 60)
+            
+            let decimalLatitude = String(format: "%.6f", decimalLat)
+            let decimalLongitude = String(format: "%.6f", decimalLon)
+            
+            return CLLocationCoordinate2D(latitude: (decimalLatitude as NSString).doubleValue, longitude: (decimalLongitude as NSString).doubleValue)
+        }
+    }
+    
+    return CLLocationCoordinate2D(latitude: 40.75773, longitude: -73.985708)
+}
+
+class LocationViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
+    @Published var authorizationStatus: CLAuthorizationStatus
+
+    private let locationManager: CLLocationManager
+
+    override init() {
+        locationManager = CLLocationManager()
+        authorizationStatus = locationManager.authorizationStatus
+
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+    }
+
+    func requestPermission() {
+        locationManager.requestWhenInUseAuthorization()
+    }
+
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        authorizationStatus = manager.authorizationStatus
+    }
+}
